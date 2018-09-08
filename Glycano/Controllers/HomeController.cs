449e06d94befa87688d8dc2d27f8d5f0a1c2bbb8
DLLIMA001 <dllima001@myuct.ac.za>
2018-09-08 18:47:27 +0200

    Removed some arguments (-i) required and CarbBuilder works

--- Glycano/Controllers/HomeController.cs
+++ Glycano/Controllers/HomeController.cs
@@ -24,23 +24,18 @@ namespace Glycano.Controllers
         [HttpPost]
         public JsonResult GetValue(Data data)
         {
-            //To run CarbBuilder
-            //Gives path to CarbBuilder and arguments before running it.
+            //To run CarbBuilder. Gives path to CarbBuilder and arguments before running it.
             ProcessStartInfo start = new ProcessStartInfo();
-            // start.FileName = Server.MapPath("~/CarbBuilder/CarbBuilder2.exe");
             start.FileName = "D:/home/site/wwwroot/CarbBuilder/CarbBuilder2.exe";
-            // start.UseShellExecute = false;
+
             //Gets sessionID unique to current browser.
             string sessionId = System.Web.HttpContext.Current.Session.SessionID;
             Data session = new Data { Value = sessionId };
 
             //Local host
             //start.Arguments = data.Value + " -o /Users/dllima001/source/repos/Gly/Glycano/Data/" + sessionId;
-            start.Arguments = data.Value + " -o /home/site/wwwroot/Data/" + sessionId;
+            start.Arguments = "-i " + data.Value + " -o /home/site/wwwroot/Data/" + sessionId;
         
-
-            //start.WindowStyle = ProcessWindowStyle.Hidden;
-            //start.CreateNoWindow = true;
             int exitCode;
 
             using (Process proc = Process.Start(start))
