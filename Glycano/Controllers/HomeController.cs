432c66f3995ca0beaa42a4f3aa3df7568fa8fb11
Moondoll5 <moondoll101@gmail.com>
2018-09-18 21:37:59 +0200

    Moved CarbBuilder code to a new function. Added comments

--- Glycano/Controllers/HomeController.cs
+++ Glycano/Controllers/HomeController.cs
@@ -34,43 +34,49 @@ namespace Glycano.Controllers
             }
             else
             {
-                //To run CarbBuilder. Gives path to CarbBuilder and arguments before running it.
-                ProcessStartInfo start = new ProcessStartInfo();
-                start.FileName = "D:/home/site/wwwroot/CarbBuilder/CarbBuilder2.exe";
-
                 //Gets sessionID unique to current browser request.
                 string sessionId = System.Web.HttpContext.Current.Session.SessionID;
-                Data session = new Data { Value = sessionId};
 
-                if (data.Repeats == null || data.Repeats == "")
-                {
-                    // Creates the arguments to give to CarbBuilder
-                    //No repeats were specified
-                    start.Arguments = "-i " + data.Value + " -o /home/site/wwwroot/Data/" + sessionId;
-                }
-                else
-                {
-                    // Creates the arguments to give to CarbBuilder
-                    start.Arguments = "-i " + data.Value + " -r " + data.Repeats + " -o /home/site/wwwroot/Data/" + sessionId;
-                }
+                //Creates an object that can be sent back to the client.
+                Data session = new Data { Value = sessionId };
 
-                int exitCode;
+                //Runs CarbBuilder
+                RunCarbBuilder(data, sessionId);
 
-                using (Process proc = Process.Start(start))
-                {
-                    proc.WaitForExit();
-                    exitCode = proc.ExitCode;
-                }
-
-                //Sends sessionID to client
-                //SessionID is used as a unique filename for PDB files on the server.
+                //Sends sessionID to client. SessionID is used as a unique filename for the new PDB file.
                 return Json(session.Value, JsonRequestBehavior.AllowGet);
             }
         }
 
-        //private void RunCarbBuilder()
-        //{
+        private void RunCarbBuilder(Data data, string sessionId)
+        {
+            //Input: User-provided arguments and the unique session ID for this request
+            //Runs CarbBuilder with user input.
 
-        //}
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = "D:/home/site/wwwroot/CarbBuilder/CarbBuilder2.exe";
+
+            //Checks if a repeat value was given
+            if (data.Repeats == null || data.Repeats == "")
+            {
+                // Creates the arguments to give to CarbBuilder
+                //No repeats were specified
+                start.Arguments = "-i " + data.Value + " -o /home/site/wwwroot/Data/" + sessionId;
+            }
+            else
+            {
+                // Creates the arguments to give to CarbBuilder
+                start.Arguments = "-i " + data.Value + " -r " + data.Repeats + " -o /home/site/wwwroot/Data/" + sessionId;
+            }
+
+            int exitCode;
+
+            //Starts CarbBuilder2.exe
+            using (Process proc = Process.Start(start))
+            {
+                proc.WaitForExit();
+                exitCode = proc.ExitCode;
+            }
+        }
     }
 }
